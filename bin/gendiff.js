7512d1e07a85245c8ab4f234f0ad8c2a434ca6b3
Panasi <flashigi@gmail.com>
2021-06-02 01:32:29 +0300

    some fixes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { cwd } from 'process';
 const program = new Command();
 program
 	.description('Compares two configuration files and shows a difference.')
 	.version('0.0.1')
-  .helpOption('-h, --help', 'output usage information')
+	.helpOption('-h, --help', 'output usage information')
 	.option('-f, --format [type]',  'output format')
-	.arguments('<filepath1> <filepath2>');
+	.arguments('<filepath1> <filepath2>')
+	.action((filepath1, filepath2) => {
+    const path1 = readFileSync(resolve(cwd(), filepath1));
+		const path2 = readFileSync(resolve(cwd(), filepath2));
+		const data1 = JSON.parse(path1);
+		const data2 = JSON.parse(path2);
+		console.log(resolve(cwd(), filepath1));
+		console.log(resolve(cwd(), filepath2));
+		console.log(data1);
+		console.log(data2);
+  });
 program.parse(process.argv);
+
